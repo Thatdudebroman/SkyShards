f2053d7edf679512bcc2a4c5dc6429e4a07261b8
Thatdudebroman <150631822+Thatdudebroman@users.noreply.github.com>
2026-08-23 20:34:20 -0400

    feat: export Bazaar arbitrage service

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,6 +2,7 @@ export { CalculationService } from "./calculationService";
 export { buildCalculationParams } from "./calculationParams";
 export { InvCalculationService } from "./invCalculationService";
 export * from "./dataService";
+export { BazaarArbitrageService } from "./bazaarArbitrageService";
 export { hypixelService } from "./hypixelService";
 export type {
   ShardOwned,
